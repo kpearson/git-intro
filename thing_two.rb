69e6dfb1d58066077190780832cc46b43b51661d
Kit <dev_kit@icloud.com>
2014-10-30 10:23:49 -0600

    new commit

---
+++ thing_two.rb
@@ -0,0 +1,120 @@
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+module ThingTwo
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
